15e9ae1738a0d017906869ab753c7049cd8ee1f9
Guy Gastineau <strings.stringsandstrings@gmail.com>
2024-12-13 17:10:42 -0500

    First half of the problem is completed.

--- solve.lisp
+++ solve.lisp
@@ -27,3 +27,56 @@
           (print b)
           (if (and (integerp a) (integerp b)) (make-solution :a a :b b)
               (make-solution :a 0 :b 0))))))
+
+;; Read until c is encountered setting the new stream position to just after c.
+(defun read-through (c in)
+  (loop for d = (read-char in nil nil)
+        until (or (null d) (eq c d))))
+
+(defun vec-initp (v)
+  (and (vec-x v) (vec-y v)))
+
+;; HERE is the format of our input data:
+;;
+;; Button A: X+55, Y+84
+;; Button B: X+64, Y+29
+;; Prize: X=6049, Y=5045
+;;
+;; What a horrible format. Who ever thought this was a good idea?  I hope they choke on worms.
+;; Anyway, let's think up a good way to parse it.  We'll go ahead and assume that all records are complete.
+;; It's the least we can do for ourselves given how horrible this format is.
+(defun parse-machine (in)
+  (let ((button-a (make-vec))
+        (button-b (make-vec))
+        (prize (make-vec)))
+    (flet ((read* () (read in nil nil)))
+      (read-through #\+ in)
+      (setf (vec-x button-a) (read*))
+      (read-through #\+ in)
+      (setf (vec-y button-a) (read*))
+      (read-through #\+ in)
+      (setf (vec-x button-b) (read*))
+      (read-through #\+ in)
+      (setf (vec-y button-b) (read*))
+      (read-through #\= in)
+      (setf (vec-x prize) (read*))
+      (read-through #\= in)
+      (setf (vec-y prize) (read*))
+      ;; That was gross!
+      (if (and (vec-initp button-a) (vec-initp button-b) (vec-initp prize))
+          `((a . ,button-a) (b . ,button-b) (prize . ,prize))
+          nil))
+    ))
+
+(defun solve (filename)
+  (with-open-file (in filename
+                      :external-format :utf-8
+                      :direction :input)
+    (loop for machine = (parse-machine in)
+          until (null machine)
+          for solution = (solve-machine (cdr (assoc 'a machine))
+                                    (cdr (assoc 'b machine))
+                                    (cdr (assoc 'prize machine)))
+          sum (+ (* 3 (solution-a solution)) (solution-b solution)) into cost
+          finally (return cost)
+          )))
